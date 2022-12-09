cf0e231b062197f2f0db5a22f3af913708a6d632
Aleksey <anonymous.aleksey@gmail.com>
2022-12-09 13:14:37 +0300

    migrations

---
+++ database/migrations/2022_12_08_133347_create_telegram_users_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTelegramUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('telegram_users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->bigInteger('telegram_id')->unique();
+            $table->string('habitica_id');
+            $table->string('habitica_key', 1000);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('telegram_users');
+    }
+}
